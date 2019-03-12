2429384fb878714af96cf007f7f1fa863e7b8b20
mamba <1612786059@qq.com>
2019-03-13 00:27:31 +0800

    Header组件

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="header">
+      <img src="../assets/cnodejs_light.svg" alt="">
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+ 
+</script>
+<style scoped>
+.header{
+    background-color: #5a5555;
+    height: 50px;
+}
+img{
+    max-width: 120px;
+    margin-left: 50px;
+    margin-top: 10px;
+}
+ul{
+    list-style: none;
+    float:right;
+    margin: 4px;
+}
+li{
+    display:inline-block;
+    padding: 10px 15px;
+}
+a{
+    text-decoration: none;
+    color: #ccc;
+    font-size: 14px;
+}
+</style>
+
