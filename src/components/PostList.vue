2dab4a4af5db759f98a555450a9550c45c634c60
mamba <1612786059@qq.com>
2019-04-30 15:25:49 +0800

    修改分类样式

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -13,11 +13,11 @@
       <ul>
         <li>
           <div class="toobar">
-            <span>全部</span>
-            <span @click="alert">精华</span>
-            <span @click="alert">分享</span>
-            <span @click="alert">问答</span>
-            <span @click="alert">招聘</span>
+            <span @click="changeTab('')" :class="{active:tabString===''}">全部</span>
+            <span @click="changeTab('good')" :class="{active:tabString==='good'}">精华</span>
+            <span @click="changeTab('share')" :class="{active:tabString==='share'}">分享</span>
+            <span @click="changeTab('ask')" :class="{active:tabString==='ask'}">问答</span>
+            <span @click="changeTab('job')" :class="{active:tabString==='job'}">招聘</span>
           </div>
         </li>
         <li v-for="post in posts">
@@ -32,11 +32,14 @@
           <span class='reply '>
             <span class="reply_count">{{post.reply_count}}</span>
             <span>/</span>
-            <span class="count_of_visits">{{post.visit_count}}</span> 
+            <span class="count_of_visits">{{post.visit_count}}</span>
           </span>
           <!--帖子的分类-->
-          <span :class="[{put_good:(post.good ==true),put_top:(post.top == true),
-           'topiclist-tab':(post.good !=true && post.top !=true)}]">
+          <span
+            class="all"
+            :class="[{put_good:(post.good ==true),put_top:(post.top == true),put_tab:(post.tab == 'ask'),
+           'topiclist-tab':(post.good !=true && post.top !=true &&post.tab != 'ask')}]"
+          >
             <span>
               {{post | tabFormatter}}
             </span>
@@ -75,7 +78,8 @@ export default {
     return {
       isLoading: false,
       posts: [], //代表页面列表数组
-      postpage: 1
+      postpage: 1,
+      tabString: ""
     };
   },
   components: {
@@ -88,7 +92,8 @@ export default {
           params: {
             //get请求要用params
             page: this.postpage,
-            limit: 20
+            limit: 20,
+            tab: this.tabString
           }
         })
         .then(res => {
@@ -104,8 +109,9 @@ export default {
       this.postpage = value;
       this.getData();
     },
-    alert() {
-      alert(`由于该页面与 '全部' 页面一致，暂时未做`);
+    changeTab(value) {
+      this.tabString = value;
+      this.getData();
     }
   },
   beforeMount() {
@@ -116,6 +122,10 @@ export default {
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
 .PostList {
   background-color: #e1e1e1;
 }
@@ -158,9 +168,6 @@ li:last-child:hover {
 li span {
   line-height: 30px;
 }
-li span:first-child {
-  width: 70px;
-}
 .allcount {
   width: 70px;
   display: inline-block;
@@ -171,40 +178,70 @@ li span:first-child {
   width: 70px;
   display: inline-block;
   text-align: center;
+  height: 30px;
+  white-space: nowrap;
 }
 .reply_count {
   color: #9e78c0;
   font-size: 14px;
 }
-.count_of_visits{
+.count_of_visits {
   font-size: 10px;
-  color: rgb(180,180,180)
+  color: rgb(180, 180, 180);
 }
-.put_good,
+
+.put_good {
+  width: 28px;
+  height: 24px;
+  padding: 3px 3px 3px 5px;
+  background-color: rgba(255, 73, 73, 0.1);
+  border: 1px solid rgba(255, 73, 73, 0.2);
+  color: #ff4949;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  font-size: 12px;
+}
 .put_top {
-  background: #80bd01;
-  padding: 2px 4px;
+  width: 28px;
+  height: 24px;
+  padding: 3px 3px 3px 5px;
+  background-color: rgba(18, 206, 102, 0.1);
+  color: #13ce66;
+  border: 1px solid rgba(18, 206, 102, 0.2);
   border-radius: 3px;
   -webkit-border-radius: 3px;
   -moz-border-radius: 3px;
   -o-border-radius: 3px;
-  color: #fff;
   font-size: 12px;
-  margin-right: 10px;
 }
-
+.put_tab {
+  width: 28px;
+  height: 24px;
+  padding: 3px 3px 3px 5px;
+  background-color: #e5e9f2;
+  border: 1px solid #e5e9f2;
+  color: #475669;
+  border-radius: 3px;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  -o-border-radius: 3px;
+  font-size: 12px;
+}
 .topiclist-tab {
-  background-color: #e5e5e5;
-  color: #999;
-  padding: 2px 4px;
+  width: 28px;
+  height: 24px;
+  padding: 3px 3px 3px 5px;
+  background-color: rgba(32, 159, 255, 0.1);
+  color: #20a0ff;
+  border: 1px solid rgba(32, 159, 255, 0.2);
   border-radius: 3px;
   -webkit-border-radius: 3px;
   -moz-border-radius: 3px;
   -o-border-radius: 3px;
   font-size: 12px;
-  margin-right: 10px;
 }
-
 .last_reply {
   text-align: right;
   min-width: 50px;
@@ -216,31 +253,37 @@ li span:first-child {
 }
 
 .toobar {
-  height: 40px;
+  height: 60px;
   background-color: #f5f5f5;
+  line-height: 60px;
 }
 
 .toobar span {
   font-size: 14px;
   color: #80bd01;
   line-height: 40px;
+  height: 40px;
   margin: 0 10px;
   cursor: pointer;
 }
-
-.toobar span:hover {
-  color: #9e78c0;
+.toobar span.active {
+  background-color: #80bd01;
+  color: #fff;
+  padding: 1px 4px;
+  border-radius: 3px;
 }
-
 a {
   text-decoration: none;
   color: black;
+  margin-left: 20px;
+  height: 30px;
 }
-
 a:hover {
   text-decoration: underline;
 }
-
+a span {
+  height: 30px;
+}
 .loading {
   text-align: center;
   padding-top: 300px;
