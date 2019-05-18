2e1f966be78ed0d6efb372d31f9d8725957f152a
mamba <1612786059@qq.com>
2019-05-18 12:07:03 +0800

    image位置调整

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,13 +3,11 @@
     <router-link :to="{name:'root'}">
      <img src="../assets/cnodejs_light.svg">
     </router-link>
-    <ul>
-      <router-link :to="{name:'root'}"><li><a href="#">首页</a></li> </router-link>
-      <li><a href="#">新手入门</a></li>
-      <li><a href="#">API</a></li>
-      <li><a href="#">关于</a></li>
-      <li><a href="#">注册</a></li>
-      <li><a href="#">登录</a></li>
+    <ul class="title">
+      <router-link :to="{name:'root'}"><li>首页</li> </router-link>
+      <li>新手入门</li>
+      <router-link :to="{name:'interface'}"><li>API</li> </router-link>
+      <router-link :to="{name:'about'}"><li>关于</li> </router-link>
     </ul>
   </div>
 </template>
@@ -27,22 +25,23 @@ export default {
 }
 img{
     max-width: 120px;
-    margin-left: 50px;
+    margin-left: 150px;
     margin-top: 10px;
 }
 ul{
     list-style: none;
     float:right;
     margin: 4px;
-}
+    padding-right: 150px;}
 li{
     display:inline-block;
     padding: 10px 15px;
-}
-a{
     text-decoration: none;
     color: #ccc;
     font-size: 14px;
+}
+.title li:hover{
+     color: #80bd01;
 }
 </style>
 
